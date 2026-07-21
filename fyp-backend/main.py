f7c7bdb13f8bc82ea1d87ee57ebe67bf82674dd9
LowYoongQian <lowyq-wm22@student.tarc.edu.my>
2026-07-22 00:44:19 +0800

    Requirement and Railway Deployment

--- fyp-backend/main.py
+++ fyp-backend/main.py
@@ -264,3 +264,9 @@ def get_public_announcements():
 @app.get("/")
 def root():
     return {"status": "online", "message": "Smart Attendance API is running"}
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
