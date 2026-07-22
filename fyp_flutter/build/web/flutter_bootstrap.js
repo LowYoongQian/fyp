075fbb3fa267a6e53293e5263869158022ff3aef
LowYoongQian <lowyq-wm22@student.tarc.edu.my>
2026-07-22 23:36:27 +0800

    Fix login URL by enforcing production HTTPS endpoint https://fyps.up.railway.app for Web targets

--- fyp_flutter/build/web/flutter_bootstrap.js
+++ fyp_flutter/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3302664149" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2517589294" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
